0396405605c435e9e8a48b98df5ea204d654aa90
huangchunwu <huangchunwu@tuniu.com>
2019-03-20 16:53:54 +0800

    加入AOP，加入MDC来支持日志的全链路跟踪

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.domain.Language;
 import com.example.demo.domain.UserEntity;
 import com.example.demo.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,14 @@ import java.util.List;
 @RestController
 public class HelloWorldController {
 
+    Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Autowired
     private IUserService userService;
 
     @RequestMapping("/hello")
     public String index(Language language) {
+        logger.info("asasasaas");
         return "Hello World";
     }
     @RequestMapping("/getAll")
